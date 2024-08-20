4135bd35980b089a9020518a27e637dbee5da6b4
GuilhermeHS2 <guilhermehsantos00@gmail.com>
2024-08-19 23:18:25 -0300

    Ajustando a resposividade do site

--- portifolio/src/app/page.tsx
+++ portifolio/src/app/page.tsx
@@ -3,15 +3,15 @@ import AboutMe from "@/components/AboutMe";
 
 export default function Home() {
     return( 
-    <div className="container mx-auto mt-8 ">
-        <div className="grid grid-cols-3 gap-24">
-            <div>
-                <Sidebar/>
-            </div>
+        <div className="container mx-auto mt-8 px-4 overflow-hidden"> 
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-24">
+                <div>
+                    <Sidebar/>
+                </div>
                 <div className="col-span-2">
                     <AboutMe/>
                 </div>
+            </div>
         </div>
-    </div>
     );
 }
